ed40bde88fd67ef1a2dbf30d9eb281bf13cfa41f
NosovArtem <nosovartem1992@gmail.com>
2023-02-08 23:11:15 +0300

    first deploy

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-scp target/fincost-1.0-SNAPSHOT.jar root@185.229.66.23:/root/bot/fincost
-scp run.sh root@185.229.66.23:/root/bot/fincost
-scp src/main/resources/prod.properties root@185.229.66.23:/root/bot/fincost
