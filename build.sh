4a1d30665214e040838a6199bdc6f887fc7c54db
NosovArtem <nosovartem1992@gmail.com>
2023-02-08 21:56:48 +0300

    first deploy

---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+scp target/fincost-1.0-SNAPSHOT.jar root@111.111.11.11:/root/bot/fincost
+scp run.sh root@111.111.11.11:/root/bot/fincost
--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-export FIN_BOT_TOKEN=
+export FIN_BOT_TOKEN=6051161505:AAHISNCoHjJzisT1fC5T0J83rOOgZFyeNmc
+
 echo Starting...
 java -jar target/fincost-1.0-SNAPSHOT.jar prod &
